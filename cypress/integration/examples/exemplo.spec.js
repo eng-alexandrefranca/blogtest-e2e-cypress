7ed347fe27bc0cd4fb617af760f6798efab91ccd
unknown <alexandredasilvafranca@hotmail.com>
2020-12-10 20:50:56 -0300

    entrega de projeto

--- cypress/integration/examples/exemplo.spec.js
+++ cypress/integration/examples/exemplo.spec.js
@@ -1,7 +1,7 @@
-describe ('Pimeiro Teste', () => {
+describe ('Primeiro Teste', () => {//discribe from Mocha: Biblioteca Básica de roteiro de teste.
     it('Exemplos Cypress', () => {
         cy.visit('https://example.cypress.io');
-        expect (true).to.equal(true)
+        expect (true).to.equal(true)//expect from Chai: Biblioteca de acertividade do teste (certo/errado)
     })
 
 
